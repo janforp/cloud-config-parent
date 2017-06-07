0d91d50fee85a70c838bac8fc70e0ae091d6bcfc
janforp <janforp@163.com>
2017-06-07 22:02:50 +0800

    Initial commit

---
+++ config-server/src/main/java/com/janita/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.janita.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Janita on 2017/6/7- 21:17
+ * 该类是:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
