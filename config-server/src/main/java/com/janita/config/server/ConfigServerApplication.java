069f9514e3ecb10901b5736f1484b3a8e3b2e725
janforp <janforp@163.com>
2017-06-07 22:43:12 +0800

    config

--- config-server/src/main/java/com/janita/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/janita/config/server/ConfigServerApplication.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Janita on 2017/6/7- 21:17
- * 该类是:
+ * 该类是:分布式项目配置文件的服务项目
+ * 当然该项目也是一个普通的springBoot项目,也可以提供其他的接口服务
  */
 @SpringBootApplication
 @EnableConfigServer
