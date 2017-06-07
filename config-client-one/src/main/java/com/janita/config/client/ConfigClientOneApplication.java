458506b087e80723756c2608a5c3528e5bf98021
janforp <janforp@163.com>
2017-06-07 22:11:06 +0800

    config

---
+++ config-client-one/src/main/java/com/janita/config/client/ConfigClientOneApplication.java
@@ -0,0 +1,17 @@
+package com.janita.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/7- 22:05
+ * 该类是:
+ */
+@SpringBootApplication
+public class ConfigClientOneApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigClientOneApplication.class, args);
+    }
+}
